761beced30bc4b5998dd570c23568fc51134ec60
Chris <cristian_smira@hotmail.ca>
2020-05-06 18:04:03 -0400

    Updates

---
+++ precache-manifest.52ce839e76447a192d545035f4db515c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d124587d6111c67650ed5c10a87c80e4",
+    "url": "/restaurant-finder/index.html"
+  },
+  {
+    "revision": "bc76e897e16fba28a1e3",
+    "url": "/restaurant-finder/static/css/main.e7cc56d0.chunk.css"
+  },
+  {
+    "revision": "6c21b8083235514cdc07",
+    "url": "/restaurant-finder/static/js/2.ea29d10e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/restaurant-finder/static/js/2.ea29d10e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc76e897e16fba28a1e3",
+    "url": "/restaurant-finder/static/js/main.e456dfec.chunk.js"
+  },
+  {
+    "revision": "9e8b728611b14bf7c301",
+    "url": "/restaurant-finder/static/js/runtime-main.2b3f8976.js"
+  }
+]);
